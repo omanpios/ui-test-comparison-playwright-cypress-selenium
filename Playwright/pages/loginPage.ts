e54e15c9302080313601232e1ac15b437e51eb47
Omar <omanpios88@gmail.com>
2025-09-02 10:46:10 -0500

    Move loginPages to its directory

--- Playwright/pages/loginPage.ts
+++ Playwright/pages/loginPage.ts
@@ -1,16 +1,18 @@
-import { Locator, Page } from "@playwright/test";
+import { type Locator, type Page, expect } from "@playwright/test";
 
 export class LoginPage {
   readonly page: Page;
   private readonly usernameInput: Locator;
   private readonly passwordInput: Locator;
   private readonly loginButton: Locator;
+  private readonly errorMessage: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.usernameInput = page.getByPlaceholder("username");
     this.passwordInput = page.getByPlaceholder("password");
-    this.loginButton = page.getByRole("button", {name: "Login"});
+    this.loginButton = page.getByRole("button", { name: "Login" });
+    this.errorMessage = page.locator("[data-test='error']")
   }
 
   async goto() {
@@ -21,5 +23,9 @@ export class LoginPage {
     await this.usernameInput.fill(username);
     await this.passwordInput.fill(password);
     await this.loginButton.click();
+  }
+
+  async errorMessageIs(expectedErrorMessage: string) {
+    await expect(this.errorMessage).toHaveText(expectedErrorMessage)
   }
 }
